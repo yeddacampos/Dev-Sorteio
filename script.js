69bfb1c0322627844788111b4c76307b4b79e6aa
Yedda <yeddaleticia100@gmail.com>
2023-11-07 23:56:45 -0400

    implementação

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function sortear () {
     const min = Math.ceil(document.querySelector('#min').value)
     const max = Math.floor(document.querySelector('#max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
-
-    alert(result)
+    if (max >= min){
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+        alert(result)
+    } else {
+        alert('O valor máximo precisa ser igual o maior que o valor mínimo')
+    }
 }
